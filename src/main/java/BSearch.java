673061fcf51c4d633eb0a32607b1fdc1ee12b2cd
Aryaman Singh <52492124+Aryaman11@users.noreply.github.com>
2024-05-08 03:55:41 +0000

    structure changes

--- src/main/java/BSearch.java
+++ src/main/java/BSearch.java
@@ -1,15 +1,15 @@
 class BSearch {
 
-    public static void BinarySearch(int Arr[], int target){
+    public static void BinarySearch(int Arr[], int target) {
         int start = 0;
-        int end = Arr.length - 1 ;
+        int end = Arr.length - 1;
 
-        while(start <= end){
-            int middle = (start + end) / 2 ; 
-            if(Arr[middle] == target){
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            if (Arr[middle] == target) {
                 System.out.println("Index" + middle);
                 return;
-            } else if(Arr[middle] > target){
+            } else if (Arr[middle] > target) {
                 end = middle - 1;
             } else {
                 start = middle + 1;
@@ -18,9 +18,8 @@ class BSearch {
         System.out.println("Element not found ---> ");
     }
 
-
     public static void main(String[] args) {
-        int Arr[] = {1,2,3,4,5,6,7,8,9,10};
-        BinarySearch(Arr,10);
+        int Arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        BinarySearch(Arr, 10);
     }
 }
