47e2741f248f1caea021e85972bedfbb1d4289a5
Aryaman Singh <52492124+Aryaman11@users.noreply.github.com>
2024-05-20 18:09:04 +0000

    anagram brut

---
+++ src/main/java/anagramBrut.java
@@ -0,0 +1,31 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String X = scan.nextLine();
+        String Y = scan.nextLine();
+        String result = anagramCheck(X,Y);
+        System.out.println(result);
+    }
+
+    public static String anagramCheck(String X , String Y){
+        char arrA[] = X.toCharArray();
+        char arrB[] = Y.toCharArray();
+        int aLen = arrA.length;
+        int bLen = arrB.length;
+        if(aLen == bLen){
+            for(int i=0;i<aLen;i++){
+            if(arrA[i] != arrB[aLen-i-1]){
+                return "No";
+            }
+        }
+        }else{
+            return "NO";
+        }
+
+        return "Yes";
+    }
+}
