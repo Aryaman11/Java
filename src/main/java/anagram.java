827540f5ff8a247814983a7dd26cca3a961d05df
Aryaman Singh <52492124+Aryaman11@users.noreply.github.com>
2024-05-20 18:32:53 +0000

    anagram

---
+++ src/main/java/anagram.java
@@ -0,0 +1,26 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String A = scan.nextLine();
+        String B = scan.nextLine();
+        String result = anagram(A,B);
+        System.out.println(result);
+
+    }
+    public static String anagram(String A , String B){
+        int demo[] = new int[256];
+        for(int i=0;i<A.length();i++){
+            demo[A.charAt(i)]++;
+            demo[B.charAt(i)]--;
+        }
+        for(int j=0;j<demo.length;j++){
+            if(demo[j] != 0){
+                return "NO";
+            }
+        }
+        return "Yes";
+    }
+}
